1f4113dbbecff68742848e18246e2a04ade9ba6e
iengu <iengu.com@gmail.com>
2021-08-31 15:30:54 +0800

    Lesson 01 finished...

---
+++ objects/Jumper.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var velocity = Vector2(100, 0)
+var jump_speed = 1000
+var target = null
+
+func _unhandled_input(event):
+	if target and event is InputEventScreenTouch and event.pressed:
+		jump()
+
+func jump():
+	target = null
+	velocity = transform.x * jump_speed
+	
+# 区域进入信号
+func _on_Jumper_area_entered(area):
+	target = area
+	velocity = Vector2.ZERO
+	
+func _physics_process(delta):
+	if target:
+		transform = target.orbit_position.global_transform
+	else:
+		position += velocity * delta
+		
+		
